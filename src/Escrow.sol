e791d0a8020988c2550272a310245c513b411375
Y0sefTamer <programmeryosef@gmail.com>
2026-07-30 06:07:02 +0300

    feat: Add state variables

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.8.27;
 
 
 contract Escrow {
-    
+    address public buyer;
+    address public seller;
+    address public arbiter;
+
+    uint256 public amount;
+
+    bool public buyerApproved;
+    bool public sellerApproved;
+
+    bool public isDisputedRaised;
 }
